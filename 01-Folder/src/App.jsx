8a03442f4ba084818a86f01a9c6b3d942d88932b
Sahil Lathotra <Bhagatsahil495@gmail.com>
2025-11-18 13:03:52 +0530

    first commit

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return (
+    <>
+      <div id="Parent">
+      <h1 id = "child1">Welcome</h1>
+      <h3 id = "child2">Sahil</h3>
+    </div>
+    <div id = "chacha"> 
+    </div>
+    </>
+  )
+}
+
+export default App
